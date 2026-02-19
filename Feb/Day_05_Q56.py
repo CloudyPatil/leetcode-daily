a1f7826ef3073e5341519fd3ed5cfa8f0e3dc6b1
Harshal Patil <harshal007patil007@gmail.com>
2026-02-19 23:57:00 +0530

    Day_05 Q56 Merge Intervals Lvl: Medium

---
+++ Feb/Day_05_Q56.py
@@ -0,0 +1,24 @@
+# 56. Merge Intervals 
+
+class Solution(object):
+    def merge(self, intervals):
+        if len(intervals) <= 1:
+            return intervals
+
+        intervals.sort(key=lambda x: x[0])
+
+        result = []
+
+        newInterval = intervals[0]
+        result.append(newInterval)
+
+        for interval in intervals:
+            if interval[0] <= newInterval[1]:
+                newInterval[1] = max(newInterval[1], interval[1])
+            else :
+                newInterval = interval
+                result.append(newInterval)
+
+        return result            
+
+        
