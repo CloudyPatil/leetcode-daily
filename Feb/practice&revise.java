67a96d81034eedaa1d6383a82491f0c783e4bf02
Harshal Patil <harshal007patil007@gmail.com>
2026-02-24 23:48:30 +0530

    Revised the Palindrome Question previsouly solved in Java

---
+++ Feb/practice&revise.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reversed = 0; 
+       int target = x;
+        while(x>0){
+           int temp = x % 10;
+            x = x / 10;
+            reversed = reversed * 10 + temp;
+    
+        }
+       return reversed == target;
+    }
+}
